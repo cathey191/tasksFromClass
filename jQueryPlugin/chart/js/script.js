04472982faff306a594c0e9564cf2efa70a62d22
cathey191 <cathey191@hotmail.com>
2018-05-10 11:07:07 +1200

    Chart Mixed

--- jQueryPlugin/chart/js/script.js
+++ jQueryPlugin/chart/js/script.js
@@ -36,7 +36,7 @@ var myChart = new Chart(ctx, {
 });
 
 var crtx = document.getElementById("myRadarChart").getContext('2d');
-var myChart = new Chart(crtx, {
+var myRaderChart = new Chart(crtx, {
   type: 'radar',
   data: {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
@@ -61,6 +61,56 @@ var myChart = new Chart(crtx, {
         'rgba(192, 57, 43,0.7)'
       ],
       borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
+
+var cmtx = document.getElementById("myMixChart").getContext('2d');
+var mixedChart = new Chart(cmtx, {
+  type: 'bar',
+  data: {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [{
+			label: 'Bar Yellow',
+      data: [2, 9, 3, 5, 6, 3],
+			backgroundColor: [
+				'rgba(241, 196, 15,1.0)',
+				'rgba(46, 204, 113,1.0)',
+				'rgba(52, 152, 219,1.0)',
+				'rgba(155, 89, 182,1.0)',
+				'rgba(231, 76, 60,1.0)',
+				'rgba(230, 126, 34,1.0)'
+			],
+			borderColor: [
+				'rgba(243, 156, 18,1.0)',
+				'rgba(39, 174, 96,1.0)',
+				'rgba(41, 128, 185,1.0)',
+				'rgba(142, 68, 173,1.0)',
+				'rgba(192, 57, 43,1.0)',
+				'rgba(211, 84, 0,1.0)'
+      ],
+      borderWidth: 1
+    },
+		{
+			label: 'Line Red',
+      data: [8, 3, 5, 4, 7, 1],
+      backgroundColor: [
+      	'rgba(231, 76, 60,0.7)'
+      ],
+      borderColor: [
+        'rgba(192, 57, 43,0.7)'
+      ],
+      borderWidth: 1,
+			type: 'line'
     }]
   },
   options: {
