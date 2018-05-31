34d5f90e22fdbb1a807505772cfe6780e9921066
cathey191 <cathey191@hotmail.com>
2018-06-01 11:48:22 +1200

    json basics

---
+++ json/index.js
@@ -0,0 +1,16 @@
+arnoldSuits = {
+	basic: {
+		price: 100,
+		sizes: [40, 42, 44, 46]
+	},
+	tuxedo: {
+		price: 160,
+		sizes: [42, 44, 46, 48, 50]
+	},
+	amrmani: {
+		price: 400,
+		sizes: [38, 40, 42, 44, 46]
+	}
+};
+
+console.log(arnoldSuits.basic.sizes[1]);
