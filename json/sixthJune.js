f76d01a28719156cd470e461077e262386052b7e
cathey191 <cathey191@hotmail.com>
2018-06-06 11:50:09 +1200

    filter, push, pop, delete

--- json/sixthJune.js
+++ json/sixthJune.js
@@ -8,13 +8,22 @@ var arr = [
     {"name":"rabbits", "count": 4}
 ];
 
-var newArr = [];
+// var newArr = [];
 
-for (var i = 0; i < arr.length; i++) {
-	// console.log(arr[i].name);
-	if (arr[i].name === "rabbits") {
-		newArr.push(arr[i]);
-	}
-}
+// for (var i = 0; i < arr.length; i++) {
+// 	// console.log(arr[i].name);
+// 	if (arr[i].name === "rabbits") {
+// 		newArr.push(arr[i]);
+// 	}
+// }
+
+// delete newArr[0]; // array lenght stays the same
+// newArr.pop(); // deletes the last item in array
+
+
+// filters array without loop or if
+var newArr = arr.filter(function (item) {
+	return item.name === "rabbits";
+})
 
 console.log(newArr);
